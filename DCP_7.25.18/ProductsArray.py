e1758b441454773e9e0e283c0b71ddd0a9ae2e8d
Alex Barnes <barnes.778@osu.edu>
2018-07-25 14:55:47 -0400

    Changed name + updated script

---
+++ DCP_7.25.18/ProductsArray.py
@@ -0,0 +1,16 @@
+#date:7.25.18
+#LosLoboDev
+
+#returns an array such that each element @ index i of the array
+#is the product of all the numbers in the original array except
+#the one @ index i.
+def initProductsArray(originalArray):
+
+    list_of_products = []
+
+    return list_of_products
+
+
+#main program
+givenArray1 = [1, 2, 3, 4, 5]
+givenArray2 = [3, 2, 1]
